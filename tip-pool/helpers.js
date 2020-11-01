ec7e5b6eaadbeacdb9cf820ae66d298051720563
Orlando G Morales <chorly786@gmail.com>
2020-11-01 10:09:33 -0500

    "Add some arrow functions"

--- tip-pool/helpers.js
+++ tip-pool/helpers.js
@@ -1,23 +1,18 @@
 // accepts 'tipAmt', 'billAmt', 'tipPercent' and sums total from allPayments objects
-function sumPaymentTotal(type) {
+const sumPaymentTotal = (type) => {
     let total = 0;
-
     for (let key in allPayments) {
         let payment = allPayments[key];
-
         total += Number(payment[type]);
     }
-
     return total;
 }
 
 // converts the bill and tip amount into a tip percent
-function calculateTipPercent(billAmt, tipAmt) {
-    return Math.round(100 / (billAmt / tipAmt));
-}
+const calculateTipPercent = (billAmt, tipAmt) => Math.round(100 / (billAmt / tipAmt))
 
 // expects a table row element, appends a newly created td element from the value
-function appendTd(tr, value) {
+const appendTd = (tr, value) => {
     let newTd = document.createElement('td');
     newTd.innerText = value;
 
@@ -25,21 +20,17 @@ function appendTd(tr, value) {
 }
 
 // append delete button and click handler for removing server from allServers and DOM td
-function appendDeleteBtn(tr, type) {
+const appendDeleteBtn = (tr, type) => {
     let newTd = document.createElement('td');
     newTd.className = 'deleteBtn';
     newTd.innerText = 'X';
-
     newTd.addEventListener('click', removeEle);
-
     tr.append(newTd);
 }
 
-function removeEle(evt) {
+const removeEle = (evt) => {
     let ele = evt.target.closest('tr');
-
     delete allServers[ele.id];
-
     ele.parentNode.removeChild(ele);
     updateServerTable();
 }
